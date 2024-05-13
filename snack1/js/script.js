7df2231441697a2d45a34885f80a441e0e1316f9
ElisabettaMirti <belmirti@gmail.com>
2024-05-13 11:46:35 +0200

    first commit

---
+++ snack1/js/script.js
@@ -0,0 +1,13 @@
+// L'utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+let numA = Number.parseInt.prompt("Type a number");
+let numB = Number.parseInt.prompt("Type a number");
+
+if (numA > numB) {
+    console.log(numA)
+} else if (numB > numA) {
+    console.log(numB)
+}else {
+    console.log("they're the same number")
+}
