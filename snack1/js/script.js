1028a5af673d9260fde05dc7aba256f3231d1334
ElisabettaMirti <belmirti@gmail.com>
2024-05-13 11:58:34 +0200

    inserita base 10 per il parseInt nei prompt

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,8 +1,8 @@
 // L'utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore.
 
-let numA = Number.parseInt(prompt("Type a number"));
-let numB = Number.parseInt(prompt("Type another number"));
+let numA = Number.parseInt(prompt("Type a number"), 10);
+let numB = Number.parseInt(prompt("Type another number"), 10);
 
 if (numA > numB) {
     console.log(numA)
