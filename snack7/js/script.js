e349c70a08d57dbef0b8dba950e35525d5b145d1
ElisabettaMirti <belmirti@gmail.com>
2024-05-14 11:27:08 +0200

    corretto snack7

--- snack7/js/script.js
+++ snack7/js/script.js
@@ -1,10 +1,17 @@
 // Fai inserire un numero, che chiameremo N, all'utente. 
 // Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
 
-const N = number.parseInt(prompt("Type a number"));
+const N = Number.parseInt(prompt("Type a number"), 10);
 
-let newArray = [];
+
 
 for (let i=0; i < N; i++) {
-    newArray = Array.from({ length: 10},Math.floor(Math.random() * 100) + 1);
+    let newArray = [];
+
+    for (let i=0; i < 10; i++) {
+        newArray.push(Math.floor (Math.random() * 100) + 1);
+    }
+
+    console.log(newArray);
 }
+
